aa92354e295f95aadd893954bb686bdaf84f0761
Sadab Masrur <sadabmasrur08@gmail.com>
2025-02-22 21:04:36 +0600

    a

---
+++ array.py
@@ -0,0 +1,8 @@
+import array as arr
+
+array_num=arr.array("i",[1,3,5,3,7,9,3])
+print(f"Original array: {array_num}")
+
+array_num.reverse()
+print(f"Reversed array: {array_num}")
+    
---
+++ operations on set.py
@@ -0,0 +1,18 @@
+mySet={1,2,3}
+print(mySet)
+
+mySet={1.0,"Hello",(1,2,3)}
+print(mySet)
+
+mySet.add("Sadab")
+mySet.add("Liban")
+print(mySet)
+
+mySet.pop()
+mySet.pop()
+print(mySet)
+
+l1=[3,5,5,5,6,7,8,9,1,2,3]
+set1=set(l1)
+
+print(f"{set1},type: {type(set1)}")
